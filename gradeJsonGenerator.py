6b07e00a40ab9703f0155d310904475cd0ccf05c
Ling Lin <linglingithub@users.noreply.github.com>
2016-05-31 15:28:04 -0700

    update"

--- gradeJsonGenerator.py
+++ gradeJsonGenerator.py
@@ -64,7 +64,7 @@ with open (csv_name,'r') as cf:
     print headers
 
     for row in f_csv:
-        print row[0], row[1], row[2], row[3], row[4] #, row[6], row[7], row[8], row[9]
+        print row[0], row[1], row[2], row[3], row[4] , row[6], row[7], row[8], row[9]
         if row[1].startswith("Chapter "):
             chapter_id = row[1][len("Chapter ")-1:row[1].index(":")].strip()
             chapter_name = row[1][row[1].index(":")+1:].strip()
@@ -86,13 +86,13 @@ with open (csv_name,'r') as cf:
             if weight is None or weight == "":
                 weight = "0"
 
-            '''
+
             total_time_for_learning = row[6].strip()
             score_of_learning = row[7].strip()
             time_for_advance_one_level_in_proficiency = row[8].strip()
             score_of_advance_one_level_in_proficiency = row[9].strip()
-            '''
-            common_core_map = row[2].strip()
+
+            # common_core_map = row[2].strip() #-- for g2 only
 
             skill_new["skill_id"] = skill_id
             skill_new["section"] = section
@@ -118,11 +118,13 @@ with open (csv_name,'r') as cf:
                 else:
                     skill_new["testskill"] = False
 
-            '''
+
             skill_new["total_time_for_learning"] = total_time_for_learning
             skill_new["score_of_learning"] = score_of_learning
             skill_new["time_for_advance_one_level_in_proficiency"] = time_for_advance_one_level_in_proficiency
             skill_new["score_of_advance_one_level_in_proficiency"] = score_of_advance_one_level_in_proficiency
+
+            '''
             skill_new["common_core_map"] = common_core_map
             '''
 
