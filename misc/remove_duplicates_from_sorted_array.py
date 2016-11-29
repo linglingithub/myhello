7d3f86b9a02986c6ccda84bd85e5178f75d10904
Ling Lin <linglingithub@users.noreply.github.com>
2016-11-29 12:36:15 -0800

    update

---
+++ misc/remove_duplicates_from_sorted_array.py
@@ -0,0 +1,109 @@
+"""
+26. Remove Duplicates from Sorted Array
+
+Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this in place with constant memory.
+
+For example,
+Given input array nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't
+matter what you leave beyond the new length.
+
+Subscribe to see which companies asked this question
+
+Hide Tags Array Two Pointers
+Hide Similar Problems (E) Remove Element
+
+Easy
+
+"""
+
+import unittest
+
+
+class Solution(object):
+    def removeDuplicates(self, nums): #82ms, 83% ==> 79ms, 88%
+        """
+        Use a pointer to keep the end of 'good' array, another pointer to scan the rest part of the array.
+        Key points:
+        1. The good pointer is always not faster than the searching pointer.
+        2. The searcher pointer will always get a value that is equal or larger than good pointer.
+        3. If equal, then ignore and move on
+        4. If not equal (greater), put it to the end of good array, update good pointer position.
+        5. How to update good array? Better to use swap
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums: # equal to is None or len(nums)==0
+            return 0
+        good = 0
+        for i in range(1, len(nums)):
+            if nums[good] != nums[i]:
+                good += 1
+                nums[good] = nums[i]
+                #nums[good+1], nums[i] = nums[i], nums[good+1]
+                #good += 1
+        #return good  # good is the index, should +1 and return as the length
+        return good+1
+
+
+
+
+
+    def removeDuplicates_ref(self, A):
+        if len(A) == 0:
+            return 0
+        j = 0
+        for i in range(0, len(A)):
+            if A[i] != A[j]:
+                A[i], A[j + 1] = A[j + 1], A[i]
+                j = j + 1
+        return j + 1
+
+
+
+
+
+class SolutionTester(unittest.TestCase):
+    def setUp(self):
+        self.sol = Solution()
+
+    def test_case1(self):
+        nums = [1,1,2]
+        answer = 2
+        nums_answer = [1,2]
+        result = self.sol.removeDuplicates(nums)
+        self.assertEqual(answer, result)
+        self.assertEqual(nums_answer, nums[:result])
+
+
+    def test_case2(self):
+        nums = [1, 1, 2, 2, 2, 3, 3]
+        answer = 3
+        nums_answer = [1, 2, 3]
+        result = self.sol.removeDuplicates(nums)
+        self.assertEqual(answer, result)
+        self.assertEqual(nums_answer, nums[:result])
+
+    def test_case3(self):
+        nums = [1, 2, 3]
+        answer = 3
+        nums_answer = [1, 2, 3]
+        result = self.sol.removeDuplicates(nums)
+        self.assertEqual(answer, result)
+        self.assertEqual(nums_answer, nums[:result])
+
+def main():
+    suite = unittest.TestLoader().loadTestsFromTestCase(SolutionTester)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+#-*- coding:utf-8 -*-
+#coding=utf-8
---
+++ misc/remove_duplicates_from_sorted_arrayII.py
@@ -0,0 +1,109 @@
+"""
+
+80. Remove Duplicates from Sorted Array II
+
+Follow up for "Remove Duplicates":
+What if duplicates are allowed at most twice?
+
+For example,
+Given sorted array nums = [1,1,1,2,2,3],
+
+Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3. It doesn't matter
+
+what you leave beyond the new length.
+
+Subscribe to see which companies asked this question
+
+Hide Tags Array Two Pointers
+
+Medium
+
+"""
+
+import unittest
+
+
+class Solution(object):
+    def removeDuplicates2(self, nums): #62ms, 79%
+        """
+        Base on the first implementation, some improvements.
+        Starting point can be 1. (But need to check the len(nums) first otherwise wrong for short nums)
+        No need to swap, just get the right value in (here it does not require original values in list)
+
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        if len(nums) < 3:
+            return len(nums)
+        good = 1
+        cnt = 1 if nums[0] != nums[1] else 2
+        allowed = 2
+        for i in range(2, len(nums)):
+            if nums[i] == nums[good]:
+                if cnt < allowed:
+                    cnt += 1
+                    good += 1
+                    nums[good] = nums[i]
+            else:
+                cnt = 1
+                good += 1
+                nums[good] = nums[i]
+        return good + 1
+
+
+    def removeDuplicates1(self, nums): #72ms, 57% ==> 52ms, 98% if remove the swap part for only getting the good value
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        good = 0
+        cnt = 1
+        allowed = 2
+        for i in range(1, len(nums)):
+            if nums[i] == nums[good]:
+                if cnt < allowed:
+                    cnt += 1
+                    good += 1
+                    #nums[good], nums[i] = nums[i], nums[good] # ===> no need to swap
+                    nums[good] = nums[i]
+            else:
+                cnt = 1
+                good += 1
+                # nums[good], nums[i] = nums[i], nums[good]
+                nums[good] = nums[i]
+        return good + 1
+
+
+
+
+
+
+class SolutionTester(unittest.TestCase):
+    def setUp(self):
+        self.sol = Solution()
+
+    def test_case1(self):
+        nums = [1,1,1,2,2,3]
+        answer = 5
+        result = self.sol.removeDuplicates(nums)
+        nums_answer = [1,1,2,2,3]
+        self.assertEqual(answer, result)
+        self.assertEqual(nums_answer, nums[:result])
+
+
+def main():
+    suite = unittest.TestLoader().loadTestsFromTestCase(SolutionTester)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+#-*- coding:utf-8 -*-
+#coding=utf-8
