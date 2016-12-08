45509cdc0c3de1c2d6cd518e9153cc6832bcf7c1
Ling Lin <linglingithub@users.noreply.github.com>
2016-12-08 11:13:07 -0800

    update

---
+++ misc/same_tree.py
@@ -0,0 +1,73 @@
+"""
+
+100. Same Tree
+
+Given two binary trees, write a function to check if they are equal or not.
+
+Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+
+Subscribe to see which companies asked this question
+
+Hide Tags Tree Depth-first Search
+
+Easy
+
+"""
+
+import unittest
+from tree_node import TreeNode
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSameTree(self, p, q): #79ms, 2.31%; 35ms, 79%
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if p is None and q is None:
+            return True
+        elif p and q and p.val==q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else:
+            return False
+
+
+    def isSameTree_ref(self, p, q):
+        if p == None and q == None: return True
+        if p and q and p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return False
+
+
+class SolutionTester(unittest.TestCase):
+    def setUp(self):
+        self.sol = Solution()
+
+    def test_case1(self):
+        p = [1]
+        q = [1]
+        answer = True
+        result = self.sol.isSameTree(TreeNode.generate_bt_from_list(p), TreeNode.generate_bt_from_list(q))
+        self.assertEqual(answer, result)
+
+
+def main():
+    suite = unittest.TestLoader().loadTestsFromTestCase(SolutionTester)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+#-*- coding:utf-8 -*-
+#coding=utf-8
