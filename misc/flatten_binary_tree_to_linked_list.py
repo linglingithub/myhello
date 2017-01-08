d942b0c34013cb6ee04a739e1fbcd9e648535f04
Ling Lin <linglingithub@users.noreply.github.com>
2017-01-07 20:19:40 -0800

    update

--- misc/flatten_binary_tree_to_linked_list.py
+++ misc/flatten_binary_tree_to_linked_list.py
@@ -186,6 +186,129 @@ class Solution(object):
 
 本文链接：http://bookshadow.com/weblog/2016/09/02/leetcode-flatten-binary-tree-to-linked-list/
 
+
+================================================================
+
+
+// Version 1: Traverse
+public class Solution {
+    private TreeNode lastNode = null;
+
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        if (lastNode != null) {
+            lastNode.left = null;
+            lastNode.right = root;
+        }
+
+        lastNode = root;
+        TreeNode right = root.right;
+        flatten(root.left);
+        flatten(right);
+    }
+}
+
+// Version 2: Divide & Conquer
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: a TreeNode, the root of the binary tree
+     * @return: nothing
+     */
+    public void flatten(TreeNode root) {
+        flattenHelper(root);
+    }
+
+    private TreeNode flattenHelper(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.left == null && root.right == null) {
+            return root;
+        }
+
+        if (root.left == null) {
+            return flattenHelper(root.right);
+        }
+
+        if (root.right == null) {
+            root.right = root.left;
+            root.left = null; // important!
+            return flattenHelper(root.right);
+        }
+
+        // Divide
+        TreeNode leftLastNode = flattenHelper(root.left);
+        TreeNode rightLastNode = flattenHelper(root.right);
+
+        // Conquer
+        leftLastNode.right = root.right;
+        root.right = root.left;
+        root.left = null; // important!
+        return rightLastNode;
+    }
+}
+
+// version 3: Non-Recursion
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: a TreeNode, the root of the binary tree
+     * @return: nothing
+     */
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.empty()) {
+            TreeNode node = stack.pop();
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+
+            // connect
+            node.left = null;
+            if (stack.empty()) {
+                node.right = null;
+            } else {
+                node.right = stack.peek();
+            }
+        }
+    }
+}
+
+
 """
 
 
