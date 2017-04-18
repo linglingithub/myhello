0e2a0d2714ccbf79892d816c32ab58ba534d4520
Ling Lin <linglingithub@users.noreply.github.com>
2017-04-17 23:33:18 -0700

    update

--- misc/sort_colors.py
+++ misc/sort_colors.py
@@ -1,5 +1,6 @@
+#coding=utf8
 """
-Sort Colors
+75. Sort Colors
 
 Given an array with n objects colored red, white or blue, sort them so that objects of the same color are adjacent,
 
@@ -15,7 +16,8 @@ click to show follow up.
 
 Follow up:
 A rather straight forward solution is a two-pass algorithm using counting sort.
-First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total number of 0's, then 1's and followed by 2's.
+First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total number of 0's,
+then 1's and followed by 2's.
 
 Could you come up with an one-pass algorithm using only constant space?
 Subscribe to see which companies asked this question
@@ -25,6 +27,8 @@ Subscribe to see which companies asked this question
 Hide Tags Array Two Pointers Sort
 Hide Similar Problems (M) Sort List (M) Wiggle Sort (M) Wiggle Sort II
 
+Medium
+
 """
 
 
@@ -33,8 +37,54 @@ import unittest
 
 
 class Solution(object):
+    def sortColors1(self, nums): #66ms, 14%; 45ms, 68%
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        if not nums:
+            return
+        left, right = 0, len(nums)-1
+        while left < len(nums) and nums[left] == 0: #need to add index protection, see case4
+            left += 1
+        while right >= 0 and nums[right] == 2:
+            right -= 1
+
+        i = left
+        while i <= right:
+            if nums[i] == 0:
+                nums[i], nums[left] = nums[left], nums[i]
+                left += 1
+                i -= 1
+            elif nums[i] == 2:
+                nums[i], nums[right] = nums[right], nums[i]
+                right -= 1
+                i -= 1
+            i += 1
+            if i <= left:  # need this, see case 6, if after swap nums[i] is still 0/2, then
+                i = left + 1
+            if i > right:
+                break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     # http://fisherlei.blogspot.com/2013/01/leetcode-sort-colors.html
-    def sortColors(self, nums):
+    def sortColors(self, nums):  #45ms, 68%
         """
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
@@ -83,9 +133,52 @@ class SolutionTester(unittest.TestCase):
         self.sol.sortColors(nums)
         self.assertEqual(answer, nums)
 
+    def test_case4(self): #===>
+        nums = [0]
+        answer = [0]
+        self.sol.sortColors(nums)
+        self.assertEqual(answer, nums)
+
+    def test_case5(self):  #===>
+        nums = [2]
+        answer = [2]
+        self.sol.sortColors(nums)
+        self.assertEqual(answer, nums)
+
+    def test_case6(self):  #===>
+        nums = [2, 0, 0]
+        answer = [0, 0, 2]
+        self.sol.sortColors(nums)
+        self.assertEqual(answer, nums)
+
+    def test_case7(self):
+        nums = [2, 0, 0, 1]
+        answer = [0, 0, 1, 2]
+        self.sol.sortColors(nums)
+        self.assertEqual(answer, nums)
+
 def main():
     suite = unittest.TestLoader().loadTestsFromTestCase(SolutionTester)
     unittest.TextTestRunner(verbosity=2).run(suite)
 
 if __name__ == "__main__":
     main()
+
+"""
+
+
+这里要求one pass完成排序，需要利用只有数组元素只有3个数的特性，否则无法完成。排序完成后一定是0...01...12....2，所以可以扫描数组，当遇到
+0时，交换到前部，当遇到1时，交换到后部。用双指针left, right来记录当前已经就位的0序列和2序列的边界位置。
+
+假设已经完成到如下所示的状态：
+
+0......0   1......1  x1 x2 .... xm   2.....2
+              |           |               |
+            left        cur          right
+
+(1) A[cur] = 1：已经就位，cur++即可
+(2) A[cur] = 0：交换A[cur]和A[left]。由于A[left]=1或left=cur，所以交换以后A[cur]已经就位，cur++，left++
+(3) A[cur] = 2：交换A[cur]和A[right]，right--。由于xm的值未知，cur不能增加，继续判断xm。
+cur > right扫描结束。
+
+"""
