b1aff03941eab50bb51a7605a58c518ffd69a525
Ling Lin <linglingithub@users.noreply.github.com>
2017-06-05 08:53:55 -0700

    update

--- mjbeto/build_post_officeII.py
+++ mjbeto/build_post_officeII.py
@@ -74,3 +74,175 @@ if __name__ == "__main__":
 
 
 #-*- coding:utf-8 -*-
+
+"""
+
+class Coordinate {
+    int x, y;
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Solution {
+    public int EMPTY = 0;
+    public int HOUSE = 1;
+    public int WALL = 2;
+    public int[][] grid;
+    public int n, m;
+    public int[] deltaX = {0, 1, -1, 0};
+    public int[] deltaY = {1, 0, 0, -1};
+    
+    private List<Coordinate> getCoordinates(int type) {
+        List<Coordinate> coordinates = new ArrayList<>();
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == type) {
+                    coordinates.add(new Coordinate(i, j));
+                }
+            }
+        }
+        
+        return coordinates;
+    }
+    
+    private void setGrid(int[][] grid) {
+        n = grid.length;
+        m = grid[0].length;
+        this.grid = grid;
+    }
+    
+    private boolean inBound(Coordinate coor) {
+        if (coor.x < 0 || coor.x >= n) {
+            return false;
+        }
+        if (coor.y < 0 || coor.y >= m) {
+            return false;
+        }
+        return grid[coor.x][coor.y] == EMPTY;
+    }
+
+    /**
+     * @param grid a 2D grid
+     * @return an integer
+     */
+    public int shortestDistance(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return -1;
+        }
+        
+        // set n, m, grid
+        setGrid(grid);
+        
+        List<Coordinate> houses = getCoordinates(HOUSE);
+        int[][] distanceSum = new int[n][m];;
+        int[][] visitedTimes = new int[n][m];;
+        for (Coordinate house : houses) {
+            bfs(house, distanceSum, visitedTimes);
+        }
+        
+        int shortest = Integer.MAX_VALUE;
+        List<Coordinate> empties = getCoordinates(EMPTY);
+        for (Coordinate empty : empties) {
+            if (visitedTimes[empty.x][empty.y] != houses.size()) {
+                continue;
+            }
+            
+            shortest = Math.min(shortest, distanceSum[empty.x][empty.y]);
+        }
+        
+        if (shortest == Integer.MAX_VALUE) {
+            return -1;
+        }
+        return shortest;
+    }
+    
+    private void bfs(Coordinate start,
+                     int[][] distanceSum,
+                     int[][] visitedTimes) {
+        Queue<Coordinate> queue = new LinkedList<>();
+        boolean[][] hash = new boolean[n][m];
+        
+        queue.offer(start);
+        hash[start.x][start.y] = true;
+        
+        int steps = 0;
+        while (!queue.isEmpty()) {
+            steps++;
+            int size = queue.size();
+            for (int temp = 0; temp < size; temp++) {
+                Coordinate coor = queue.poll();
+                for (int i = 0; i < 4; i++) {
+                    Coordinate adj = new Coordinate(
+                        coor.x + deltaX[i],
+                        coor.y + deltaY[i]
+                    );
+                    if (!inBound(adj)) {
+                        continue;
+                    }
+                    if (hash[adj.x][adj.y]) {
+                        continue;
+                    }
+                    queue.offer(adj);
+                    hash[adj.x][adj.y] = true;
+                    distanceSum[adj.x][adj.y] += steps;
+                    visitedTimes[adj.x][adj.y]++;
+                } // direction
+            } // for temp
+        } // while
+    }
+}
+
+========================================================================================================================
+
+class Solution:
+    # @param {int[][]} grid a 2D grid
+    # @return {int} an integer
+    def shortestDistance(self, grid):
+        # Write your code here
+        if not grid:
+            return 0
+        m = len(grid)
+        n = len(grid[0])
+        
+        dist = [[sys.maxint for j in range(n)] for i in range(m)]
+        reachable_count = [[0 for j in range(n)] for i in range(m)]
+        min_dist = sys.maxint
+        
+        buildings = 0
+        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    self.bfs(grid, i, j, dist, m, n, reachable_count)
+                    buildings += 1
+  
+        for i in range(m):
+            for j in range(n):
+                if reachable_count[i][j] == buildings and dist[i][j] < min_dist:
+                    min_dist = dist[i][j]
+        return min_dist if min_dist != sys.maxint else -1
+        
+    def bfs(self, grid, i, j, dist, m, n, reachable_count):
+        visited = [[False for y in range(n)] for x in range(m)]
+        visited[i][j] = True
+        q = collections.deque([(i,j, 0)])
+        
+        while q:
+            i, j, l = q.popleft()
+            if dist[i][j] == sys.maxint:
+                dist[i][j] = 0
+            dist[i][j] += l
+
+            for x, y in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+                nx, ny = i+x, j+y
+
+                if nx > -1 and nx < m and ny > -1 and ny < n and not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    if grid[nx][ny] == 0:
+                        q.append((nx, ny, l+1))
+                        reachable_count[nx][ny] += 1 
+
+"""
--- mjbeto/copy_booksII.py
+++ mjbeto/copy_booksII.py
@@ -29,6 +29,36 @@ class Solution:
     # @return: an integer
     def copyBooksII(self, n, times):
         # write your code here
+        if not times:
+            return -1
+        if n < 1:
+            return 0
+        if len(times) >= n: # think about this case, case 2
+            #return sorted(times)[n-1] # can't do this , check case 2
+            times.sort()
+
+        l, r = 1, (1<<31)-1
+        while l < r:
+            mid = (l+r)/2
+            book = self.find_can_copy_books(mid, times, n)
+            if book < n:
+                l = mid+1
+            else:
+                r = mid
+        return l
+
+    def find_can_copy_books(self, total, times, n):
+        cnt = 0
+        single_time = 0
+        for i in times:
+            if i > total:
+                return n - 1
+            cnt += total / i
+            if cnt > n:
+                return cnt
+        return cnt
+
+
 
 
 
@@ -40,6 +70,29 @@ class SolutionTester(unittest.TestCase):
         n =4
         nums = [3,2,4]
         answer = 4
+        result = self.sol.copyBooksII(n, nums)
+        self.assertEqual(answer, result)
+
+    def test_case3(self):
+        n = 3
+        nums = [3,2,4]
+        answer = 4
+        result = self.sol.copyBooksII(n, nums)
+        self.assertEqual(answer, result)
+
+    def test_case4(self):
+        n = 5
+        nums = [3,2,4]
+        answer = 6
+        result = self.sol.copyBooksII(n, nums)
+        self.assertEqual(answer, result)
+
+
+    def test_case2(self): #====> think of the case that far more people than book, then need to find the fastest people,
+        # when some ppl are much faster than others, need to have faster people do multiple books, some slow people do nothing
+        n = 53
+        nums = [274,233,513,981,372,328,251,926,363,542,573,966,846,940,848,22,804,881,492,151,583,371,403,135,571,37,647,517,929,915,500,372,763,94,167,100,53,679,878,463,310,568,689,631,817,820,967,40,371,589,787,891,304,496,419,64,791,768,330,678,15,2,924,958,583,919,750,63,435,858,478,221,752,238,410,510,56,475,749,329,633,844,111,978,282,561,91,327,298,301,367,402,805,50,962,91,155,84,955,280]
+        answer = 75
         result = self.sol.copyBooksII(n, nums)
         self.assertEqual(answer, result)
 
@@ -55,3 +108,64 @@ if __name__ == "__main__":
 
 
 #-*- coding:utf-8 -*-
+
+"""
+
+public class Solution {
+    /**
+     * @param n: an integer
+     * @param times: an array of integers
+     * @return: an integer
+     */
+    public int copyBooksII(int n, int[] times) {
+        int k = times.length;
+        int[][] f = new int[2][n+1];
+        for (int j = 0 ; j <= n; ++j) {
+            f[0][j] = j * times[0];
+        }
+        for (int i = 1; i < k; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                int a = i%2;
+                int b = 1-a;
+                
+                f[a][j] = Integer.MAX_VALUE;
+                for (int l = 0; l <= j; ++l) {
+                    if (f[b][j-l] > times[i] * l) {
+                        f[a][j] = Math.min(f[a][j], f[b][j-l]);
+                    } else {
+                        f[a][j] = Math.min(f[a][j], times[i] * l);
+                        break;
+                    }
+                }
+                
+            }
+        }
+        return f[(k-1)%2][n];
+    }
+    
+    # the 2D way will MLE 
+    public int copyBooksII2D(int n, int[] times) {
+        int k = times.length;
+        int[][] f = new int[k][n+1];
+        for (int j = 0 ; j <= n; ++j) {
+            f[0][j] = j * times[0];
+        }
+        for (int i = 1; i < k; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                f[i][j] = Integer.MAX_VALUE;
+                for (int l = 0; l <= j; ++l) {
+                    f[i][j] = Math.min(f[i][j], Math.max(f[i-1][j-l], times[i] * l));
+                    if (f[i-1][j-l] <= times[i] * l) {
+                        break;
+                    }
+                }
+                
+            }
+        }
+        return f[k-1][n];
+    }
+}
+
+
+
+"""
