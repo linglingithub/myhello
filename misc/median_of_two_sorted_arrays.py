5f24ae12f095194b633a5dbf753a5ee9ed449dcd
Ling Lin <linglingithub@users.noreply.github.com>
2017-06-08 16:13:41 -0700

    update

--- misc/median_of_two_sorted_arrays.py
+++ misc/median_of_two_sorted_arrays.py
@@ -36,15 +36,48 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
-        if (nums1 is None or len(nums1)==0) and (nums2 is None or len(nums2)==0):
+        if not nums1:
+            nums1, nums2 = nums2, nums1
+        #if not nums2:  # should be checking nums1 again, case 3
+        if not nums1:
             return None
-        l1 = 0
-        r1 = len(nums1) - 1
-        mid1 = (l1+r1) / 2
-        l2 = 0
-        r2 = len(nums2) - 1
-        mid2 = (l2+r2)/2
-        while l1
+        m, n = len(nums1), len(nums2)
+        if m < n:
+            nums1, nums2 = nums2, nums1
+            m, n = n, m
+        # the above makes sure nums1 is the longer array, to simplify the following process
+        mid = (m + n) / 2
+        if (m + n) % 2 == 0:
+            mid -= 1
+        i, j, tag = 0, 0, 1
+        #while i < m and j < n and i + j <= mid + 1: # should not be <= here, should be <
+        while i < m and j < n and i + j < mid + 1:
+            if nums1[i] < nums2[j]:
+                i += 1
+                tag = 1
+            else:
+                j += 1
+                tag = 2
+        if j >= n and i + j < mid + 1:
+            tag = 1
+            delta = mid + 1 - (i + j)
+            i += delta
+
+        if tag == 1:
+            result = nums1[i-1]  # should be one step backward
+        else:
+            result = nums2[j-1]  # should be one step backward
+
+        if (m + n) % 2 == 0:  # add consideration for even number, see case 2
+            #next = min(nums1[i], nums2[j]) if i < m and j < n else nums1[i] , wrong when result is at the end of nums1, case2
+            if i < m and j < n:
+                next = min(nums1[i], nums2[j])
+            elif i < m:
+                next = nums1[i]
+            else:
+                next = nums2[j]
+            result = (result + next) / 2.0
+        return result
 
 
 
@@ -52,6 +85,13 @@ class Solution(object):
 class SolutionTester(unittest.TestCase):
     def setUp(self):
         self.sol = Solution()
+
+    def test_case3(self):
+        nums1 = [2]
+        nums2 = []
+        answer = 2.0   #wrong output None
+        result = self.sol.findMedianSortedArrays(nums1, nums2)
+        self.assertEqual(answer, result)
 
     def test_case1(self):
         nums1 = [1, 3]
