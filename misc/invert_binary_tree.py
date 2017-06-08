e28ae82f53299b861d49adcba8c20ed8335467f7
Ling Lin <linglingithub@users.noreply.github.com>
2017-06-08 15:07:03 -0700

    update

---
+++ misc/invert_binary_tree.py
@@ -0,0 +1,80 @@
+#coding=utf-8
+
+import unittest
+
+"""
+
+226. Invert Binary Tree
+
+
+Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+to
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so fuck off.
+Subscribe to see which companies asked this question.
+
+Hide Tags Tree
+
+
+Easy
+
+
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        left, right = root.left, root.right
+        root.left, root.right = right, left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+
+
+class SolutionTester(unittest.TestCase):
+    def setUp(self):
+        self.sol = Solution()
+
+    def test_case1(self):
+        nums = 1
+        answer = 1
+        result = self.sol.searchInsert(nums)
+        self.assertEqual(answer, result)
+
+
+def main():
+    suite = unittest.TestLoader().loadTestsFromTestCase(SolutionTester)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+#-*- coding:utf-8 -*-
