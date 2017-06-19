30fc741ee2b734ff89b1c9cc30a7b5b5a18726d3
Ling Lin <linglingithub@users.noreply.github.com>
2017-06-18 23:43:51 -0700

    update

--- mjbeto/maximal_square.py
+++ mjbeto/maximal_square.py
@@ -96,3 +96,29 @@ if __name__ == "__main__":
 
 
 #-*- coding:utf-8 -*-
+
+"""
+
+jiujiang answer:
+
+
+    def maxSquare(self, matrix):
+        # write your code here
+        m = len(matrix)
+        n = len(matrix[0])
+        f = []
+        for i in xrange(m): f.append([])
+        for i in xrange(m):
+            for j in xrange(n): f[i].append(0)
+        for i in xrange(n): f[0][i] = matrix[0][i]
+        for i in xrange(1, m): f[i][0] = matrix[i][0]
+        for i in xrange(1, m):
+            for j in xrange(1, n): 
+                if (matrix[i][j]==0): f[i][j] = 0
+                else: f[i][j] = 1+min(f[i-1][j], min(f[i-1][j-1], f[i][j-1]))
+        ans = 0        
+        for i in xrange(m):
+            for j in xrange(n): ans = max(ans, f[i][j])
+        return ans*ans
+
+"""
