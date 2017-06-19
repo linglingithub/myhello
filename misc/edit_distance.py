88fc9c3702202db3c797dcb2ee2ea4168bea52a5
Ling Lin <linglingithub@users.noreply.github.com>
2017-06-19 14:34:51 -0700

    update

--- misc/edit_distance.py
+++ misc/edit_distance.py
@@ -26,7 +26,33 @@ Hard
 import unittest
 
 
-class Solution(object):
+class Solution:
+    # @param word1 & word2: Two string.
+    # @return: The minimum number of steps.
+    def minDistance(self, word1, word2):
+        # write your code here
+        if not word1:
+            return len(word2) if word2 else 0
+        if not word2:
+            return len(word1) if word1 else 0
+        m, n = len(word1), len(word2)
+        dp = [[-1 for _ in range(n + 1)] for _ in range(m + 1)]
+        for i in range(m + 1):
+            dp[i][0] = i
+        for j in range(n + 1):
+            dp[0][j] = j
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                a, b = word1[i-1], word2[j-1]  # index wrong here, should have -1
+                if a == b:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = 1 + min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1])
+        #return dp[m + 1][n + 1]   #wrong index
+        return dp[m][n]
+
+
+class Solution1(object):
     def minDistance(self, word1, word2):
         """
         :type word1: str
