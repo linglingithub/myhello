030f88026ed09ee56142a83d6bb661d3d1951072
Ling Lin <linglingithub@users.noreply.github.com>
2017-07-13 18:03:34 -0700

    update

--- misc/insert_interval.py
+++ misc/insert_interval.py
@@ -143,3 +143,76 @@ if __name__ == "__main__":
 
 
 #-*- coding:utf-8 -*-
+
+"""
+
+# simple and good solution in Java
+
+public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+    List<Interval> result = new LinkedList<>();
+    int i = 0;
+    // add all the intervals ending before newInterval starts
+    while (i < intervals.size() && intervals.get(i).end < newInterval.start)
+        result.add(intervals.get(i++));
+    // merge all overlapping intervals to one considering newInterval
+    while (i < intervals.size() && intervals.get(i).start <= newInterval.end) {
+        newInterval = new Interval( // we could mutate newInterval here also
+                Math.min(newInterval.start, intervals.get(i).start),
+                Math.max(newInterval.end, intervals.get(i).end));
+        i++;
+    }
+    result.add(newInterval); // add the union of intervals we got
+    // add all the rest
+    while (i < intervals.size()) result.add(intervals.get(i++)); 
+    return result;
+}
+
+
+========================================================================================
+
+# good solutions in python
+
+
+Solution 1: (7 lines, 88 ms)
+
+Collect the intervals strictly left or right of the new interval, then merge the new one with the middle ones (if any) before inserting it between left and right ones.
+
+def insert(self, intervals, newInterval):
+    s, e = newInterval.start, newInterval.end
+    left = [i for i in intervals if i.end < s]
+    right = [i for i in intervals if i.start > e]
+    if left + right != intervals:
+        s = min(s, intervals[len(left)].start)
+        e = max(e, intervals[~len(right)].end)
+    return left + [Interval(s, e)] + right
+Solution 2: (8 lines, 84 ms)
+
+Same algorithm as solution 1, but different implementation with only one pass and explicitly collecting the to-be-merged intervals.
+
+def insert(self, intervals, newInterval):
+    s, e = newInterval.start, newInterval.end
+    parts = merge, left, right = [], [], []
+    for i in intervals:
+        parts[(i.end < s) - (i.start > e)].append(i)
+    if merge:
+        s = min(s, merge[0].start)
+        e = max(e, merge[-1].end)
+    return left + [Interval(s, e)] + right
+Solution 3: (11 lines, 80 ms)
+
+Same again, but collect and merge while going over the intervals once.
+
+def insert(self, intervals, newInterval):
+    s, e = newInterval.start, newInterval.end
+    left, right = [], []
+    for i in intervals:
+        if i.end < s:
+            left += i,
+        elif i.start > e:
+            right += i,
+        else:
+            s = min(s, i.start)
+            e = max(e, i.end)
+    return left + [Interval(s, e)] + right
+
+"""
