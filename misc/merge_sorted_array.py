e6ef66ddd1a2008442a38c8c40665c283ddf3c51
Ling Lin <linglingithub@users.noreply.github.com>
2017-07-13 18:01:39 -0700

    update

--- misc/merge_sorted_array.py
+++ misc/merge_sorted_array.py
@@ -4,7 +4,8 @@ Merge Sorted Array
 Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
 
 Note:
-You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from 
+nums2.
 
 The number of elements initialized in nums1 and nums2 are m and n respectively.
 
@@ -19,7 +20,27 @@ Hide Similar Problems (E) Merge Two Sorted Lists
 import unittest
 import copy
 
+
 class Solution(object):
+    def merge(self, nums1, m, nums2, n): # ref idea, do that from end to start in nums1 array !
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        while m > 0 and n > 0:   # type: should be while, not if here !!!
+            if nums1[m-1] >= nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
+        if n > 0:
+            nums1[0:n] = nums2[0:n]
+
+class Solution1(object):
     def merge(self, nums1, m, nums2, n):
         i = 0
         j = 0
