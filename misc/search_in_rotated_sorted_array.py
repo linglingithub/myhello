737d6a1f7b3de4137c0c3a55c8e4504b91a7d68e
Ling Lin <linglingithub@users.noreply.github.com>
2017-07-26 18:18:59 -0700

    update

--- misc/search_in_rotated_sorted_array.py
+++ misc/search_in_rotated_sorted_array.py
@@ -19,7 +19,73 @@ Hide Similar Problems (M) Search in Rotated Sorted Array II (M) Find Minimum in 
 
 import unittest
 
-class Solution(object):
+
+class Solution:
+    """
+    @param A : a list of integers
+    @param target : an integer to be searched
+    @return : an integer
+    """
+
+    def search(self, A, target):
+        # write your code here
+        if not A:
+            return -1
+        left, right = 0, len(A) - 1
+        #while left < right:  # wrong here
+        while left <= right:
+            mid = (left+right)/2
+            if A[mid] == target:
+                return mid
+            if A[mid] < A[right]: # left half, include 0, mid
+                if A[mid] < target <= A[right]:
+                        left = mid + 1
+                else:
+                    right = mid - 1
+            else: # -- right half, exclude mid
+                if  A[left] <= target < A[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return -1
+
+    def search1(self, A, target):
+        """
+        Not clean enough, need to focus on when is the only situation that left, right can be adjusted.!!!
+        :param A: 
+        :param target: 
+        :return: 
+        """
+        # write your code here
+        if not A:
+            return -1
+        left, right = 0, len(A) - 1
+        # while left < right:  # wrong here
+        while left <= right:
+            mid = (left + right) / 2
+            if A[mid] == target:
+                return mid
+            if A[mid] < A[right]:
+                if A[mid] < target:  # find bigger
+                    if A[right] >= target:
+                        left = mid + 1
+                    else:
+                        right = mid - 1
+                else:  # find smaller
+                    right = mid - 1
+            else:  # -- right half, exclude mid
+                if A[mid] < target:  # find bigger
+                    left = mid + 1
+                else:  # find smaller
+                    if A[left] <= target:
+                        right = mid - 1
+                    else:
+                        left = mid + 1
+
+        return -1
+
+
+class Solution1(object):
     def search(self, nums, target): #52ms, 50%
         """
         :type nums: List[int]
@@ -65,6 +131,12 @@ class SolutionTester(unittest.TestCase):
         result = self.sol.search(nums, target)
         self.assertEqual(answer, result)
 
+    def test_case03(self):
+        nums = [1,2,3]
+        target = 1
+        answer = 0
+        result = self.sol.search(nums, target)
+        self.assertEqual(answer, result)
 
 def main():
     suite = unittest.TestLoader().loadTestsFromTestCase(SolutionTester)
