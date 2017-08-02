3ba248639d605205f1d22de4e53e12e731e278ee
Ling Lin <linglingithub@users.noreply.github.com>
2017-08-02 14:29:21 -0700

    update

---
+++ misc/lowest_common_ancestor_of_a_bst.py
@@ -0,0 +1,159 @@
+#coding=utf-8
+
+import unittest
+
+"""
+
+235. Lowest Common Ancestor of a Binary Search Tree
+
+Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and 
+w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of
+ itself).”
+
+        _______6______
+       /              \
+    ___2__          ___8__
+   /      \        /      \
+   0      _4       7       9
+         /  \
+         3   5
+For example, the lowest common ancestor (LCA) of nodes 2 and 8 is 6. Another example is LCA of nodes 2 and 
+4 is 2, since a node can be a descendant of itself according to the LCA definition.
+
+Seen this question in a real interview before?   Yes  
+
+Related Topics 
+Tree 
+Similar Questions 
+Lowest Common Ancestor of a Binary Tree 
+
+"""
+
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        Basic way is to dfs the tree and find p and q, then compare the paths of p and q and find out the LCA.
+        But in a BST, anything else to exploit to make it more efficient?
+        NOTE that given a root, all smaller numbers in left sub-tree and all bigger ones in right-subtree.
+        If, for example, p.val < root.val and q.val > root.val, then the paths split, current root will be
+        the LCA! If both are smaller or both are bigger, then choose one sub-tree and continue to check.
+        Remember to Think about when p may be LCA of p and q.
+        
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if not root or not p or not q:
+            return None
+        while root and (root.val-p.val) * (root.val-q.val) > 0:
+            root = root.left if root.val>p.val else root.right
+        return root
+
+
+
+
+class SolutionTester(unittest.TestCase):
+    def setUp(self):
+        self.sol = Solution()
+
+    def test_case1(self):
+        nums = 1
+        answer = 1
+        result = self.sol.searchInsert(nums)
+        self.assertEqual(answer, result)
+
+
+def main():
+    suite = unittest.TestLoader().loadTestsFromTestCase(SolutionTester)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+#-*- coding:utf-8 -*-
+
+"""
+
+Just walk down from the whole tree's root as long as both p and q are in the same subtree (meaning their values 
+are both smaller or both larger than root's). This walks straight from the root to the LCA, not looking at the 
+rest of the tree, so it's pretty much as fast as it gets. A few ways to do it:
+
+Iterative, O(1) space
+
+Python
+
+def lowestCommonAncestor(self, root, p, q):
+    while (root.val - p.val) * (root.val - q.val) > 0:
+        root = (root.left, root.right)[p.val > root.val]
+    return root
+Java
+
+public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    while ((root.val - p.val) * (root.val - q.val) > 0)
+        root = p.val < root.val ? root.left : root.right;
+    return root;
+}
+(in case of overflow, I'd do (root.val - (long)p.val) * (root.val - (long)q.val))
+
+Different Python
+
+def lowestCommonAncestor(self, root, p, q):
+    a, b = sorted([p.val, q.val])
+    while not a <= root.val <= b:
+        root = (root.left, root.right)[a > root.val]
+    return root
+"Long" Python, maybe easiest to understand
+
+def lowestCommonAncestor(self, root, p, q):
+    while root:
+        if p.val < root.val > q.val:
+            root = root.left
+        elif p.val > root.val < q.val:
+            root = root.right
+        else:
+            return root
+Recursive
+
+Python
+
+def lowestCommonAncestor(self, root, p, q):
+    next = p.val < root.val > q.val and root.left or \
+           p.val > root.val < q.val and root.right
+    return self.lowestCommonAncestor(next, p, q) if next else root
+Python One-Liner
+
+def lowestCommonAncestor(self, root, p, q):
+    return root if (root.val - p.val) * (root.val - q.val) < 1 else \
+           self.lowestCommonAncestor((root.left, root.right)[p.val > root.val], p, q)
+Java One-Liner
+
+public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    return (root.val - p.val) * (root.val - q.val) < 1 ? root :
+           lowestCommonAncestor(p.val < root.val ? root.left : root.right, p, q);
+}
+"Long" Python, maybe easiest to understand
+
+def lowestCommonAncestor(self, root, p, q):
+    if p.val < root.val > q.val:
+        return self.lowestCommonAncestor(root.left, p, q)
+    if p.val > root.val < q.val:
+        return self.lowestCommonAncestor(root.right, p, q)
+    return root
+
+
+"""
--- test/generator_test.py
+++ test/generator_test.py
@@ -1,5 +1,7 @@
 #coding=utf-8
 """
+(1) Examples of generator, how it works
+
 http://anandology.com/python-practice-book/iterators.html
 
 5.2. Generators
@@ -101,3 +103,78 @@ def take(n, seq):
     return result
 
 print take(5, squares()) # prints [1, 4, 9, 16, 25]
+
+
+"""
+
+(2) diff in execution of yield vs return
+
+The yield keyword is central to python generator functions but what does this yield keyword do? To understand the yield 
+keyword, we contrast it with the return key word; the other keyword that gives back control to the caller of a 
+function. When a function that is executing encounters the yield keyword, it suspends execution at that point, saves 
+its context and returns to the caller along with any value in the expression_list; when the caller invokes next on the 
+object, execution of the function continues till another yield or return is encountered or end of function is reached. 
+
+
+(3) what use for generator
+
+https://stackoverflow.com/questions/102535/what-can-you-use-python-generator-functions-for
+
+Generators give you lazy evaluation. You use them by iterating over them, either explicitly with 'for' or implicitly by 
+passing it to any function or construct that iterates. You can think of generators as returning multiple items, as if 
+they return a list, but instead of returning them all at once they return them one-by-one, and the generator function 
+is paused until the next item is requested.
+
+Generators are good for calculating large sets of results (in particular calculations involving loops themselves) where 
+you don't know if you are going to need all results, or where you don't want to allocate the memory for all results at 
+the same time. Or for situations where the generator uses another generator, or consumes some other resource, and it's 
+more convenient if that happened as late as possible.
+
+
+
+(4) more example of using generator to read large files
+
+https://www.oreilly.com/ideas/2-great-benefits-of-python-generators-and-how-they-changed-me-forever
+
+And ideally without slurping in the entire large file. Each record is split over several lines, so you have to do 
+something more sophisticated than "for line in myfile:". The nicest way I know to solve this problem in Python is 
+using a generator:
+
+def gen_records(path):
+    with open(path) as handle:
+        record = {}
+        for line in handle:
+            if line == "\n":
+                yield record
+                record = {}
+                continue
+            key, value = line.rstrip("\n").split(": ", 1)
+            record[key] = value
+That lets us do things like:
+
+for record in gen_records('data.txt'):
+    print("{} had {} requests in the past hour".format(
+        record["article"], record["requests"]))
+Someone using gen_records doesn't have to know, or care, that the source is a multiline-record format. There is 
+complexity, yes, because that's necessary in this situation. But all that complexity is very nicely encapsulated in the 
+generator function!
+
+This pattern is especially valuable with continuous streams, like from a socket connection, or tailing a log file. 
+Imagine a long-running program that listens on some source and continually working with the produced records - 
+generators are perfect for this.
+
+....
+
+And it illustrates the other benefit of generators mentioned earlier: encapsulation. It provides new and useful ways 
+for you to package and isolate internal code dependencies. You can do the same thing with classes, but only by 
+spreading state across several methods, in a way that's not nearly as easy to understand.
+
+....
+
+Compared to using (say) a simple list, iterators tremendously reduce memory footprint; improve scalability; and make 
+your code more responsive to the end user. Through this, Python taught me to be a better programmer in every language.
+
+
+
+
+"""
