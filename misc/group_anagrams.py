accc429415c4e8a2f5b07c9085bf816f935c654a
Ling Lin <linglingithub@users.noreply.github.com>
2017-08-02 20:45:13 -0700

    update

--- misc/group_anagrams.py
+++ misc/group_anagrams.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 """
 
 49. Group Anagrams
@@ -27,6 +28,24 @@ import unittest
 
 
 class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        if not strs:
+            return []
+        groups = {}
+        for anag in strs:
+            # key = sorted(anag) # TypeError: unhashable type: 'list'
+            key = "".join(sorted(anag))
+            tmp = groups.get(key, [])
+            tmp.append(anag)
+            groups[key] = tmp
+            # groups[key] = groups.get(key, []).append(anag)  #AttributeError: 'NoneType' object has no attribute 'append'
+        return groups.values()
+
+class Solution1(object):
     def groupAnagrams(self, strs):
         """
         :type strs: List[str]
@@ -83,3 +102,32 @@ if __name__ == "__main__":
 
 #-*- coding:utf-8 -*-
 #coding=utf-8
+
+
+"""
+
+Java beat 100%!!! use prime number
+public static List<List<String>> groupAnagrams(String[] strs) { 
+   int[] prime = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103};//最多10609个z
+    
+            List<List<String>> res = new ArrayList<>();
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (String s : strs) {
+                int key = 1;
+                for (char c : s.toCharArray()) {
+                    key *= prime[c - 'a'];
+                }
+                List<String> t;
+                if (map.containsKey(key)) {
+                    t = res.get(map.get(key));
+                } else {
+                    t = new ArrayList<>();
+                    res.add(t);
+                    map.put(key, res.size() - 1);
+                }
+                t.add(s);
+            }
+            return res;
+    }
+
+"""
