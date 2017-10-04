4d8c46d3c57d21a379756d10ae68e42c81feb1c4
Ling Lin <linglingithub@users.noreply.github.com>
2017-10-03 23:46:47 -0700

    update

---
+++ freq/odd_even_linked_list.py
@@ -0,0 +1,93 @@
+#coding=utf-8
+
+import unittest
+
+"""
+328. Odd Even Linked List
+
+Given a singly linked list, group all odd nodes together followed by the even nodes. Please note here we are talking about the node number and not the value in the nodes.
+
+You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+
+Example:
+Given 1->2->3->4->5->NULL,
+return 1->3->5->2->4->NULL.
+
+Note:
+The relative order inside both the even and odd groups should remain as it was in the input. 
+The first node is considered odd, the second node even and so on ...
+
+Credits:
+Special thanks to @DjangoUnchained for adding this problem and creating all test cases.
+
+Difficulty:Medium
+Total Accepted:76.1K
+Total Submissions:173.5K
+Contributor: LeetCode
+Subscribe to see which companies asked this question.
+
+Related Topics 
+Linked List 
+
+"""
+
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def oddEvenList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        dummy1 = odd = ListNode(-1)
+        dummy2 = even = ListNode(-1)
+        while head:
+            odd.next = head
+            odd = head
+            even.next = odd.next
+            even = even.next
+            head = even.next if even else None
+        odd.next = dummy2.next
+        return dummy1.next
+
+
+class SolutionTester(unittest.TestCase):
+    def setUp(self):
+        self.sol = Solution()
+
+    def test_case1(self):
+        nums = 1
+        answer = 1
+        result = self.sol.searchInsert(nums)
+        self.assertEqual(answer, result)
+
+
+def main():
+    suite = unittest.TestLoader().loadTestsFromTestCase(SolutionTester)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+if __name__ == "__main__":
+    main()
+
+"""
+def oddEvenList(self, head):
+    dummy1 = odd = ListNode(0)
+    dummy2 = even = ListNode(0)
+    while head:
+        odd.next = head
+        even.next = head.next
+        odd = odd.next
+        even = even.next
+        head = head.next.next if even else None
+    odd.next = dummy2.next
+    return dummy1.next
+"""
+
+#-*- coding:utf-8 -*-
