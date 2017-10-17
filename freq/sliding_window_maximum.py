f68eae06c4137a23bf7845ca1ec0e740765ff0d2
Ling Lin <linglingithub@users.noreply.github.com>
2017-10-16 22:56:54 -0700

    update

--- freq/sliding_window_maximum.py
+++ freq/sliding_window_maximum.py
@@ -4,6 +4,8 @@ import unittest
 
 """
 
+239.
+
 Sliding Window Maximum
 
 Given an array of n integer with duplicate number, and a moving window(size k), move the window at each iteration from 
@@ -38,8 +40,64 @@ Hard Paint House II
 
 """
 
+class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        if not nums or k <= 0:
+            return []
+        from collections import deque
+        queue = deque()
+        res = []
+        n = len(nums)
+        for i in range(k):
+            #while queue and nums[queue[0]] < nums[i]:  # should be queue[-1] here, otherwise wrong
+            while queue and nums[queue[-1]] < nums[i]:
+                queue.pop()
+            queue.append(i)
+        res.append(nums[queue[0]])
+        for i in range(k, n):
+            wleft = i-k+1
+            if queue and queue[0] < wleft:
+                queue.popleft()
+            while queue and nums[queue[-1]] < nums[i]:
+                queue.pop()
+            queue.append(i)
+            res.append(nums[queue[0]])
+        return res
 
-class Solution:
+    def maxSlidingWindow1(self, nums, k):
+        """
+        can actually go without wmax, cause the deque is saving decreasing numbers, so queue[0] is always current max
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        if not nums or k <= 0:   # protection for case3
+            return []
+        from collections import deque
+        queue = deque()
+        res = []
+        wmax = nums[0]
+        for i in range(k):
+            wmax = max(wmax, nums[i])
+            while queue and nums[queue[-1]] < nums[i]:
+                queue.pop()
+            queue.append(i)
+        res.append(wmax)
+        n = len(nums)
+        for i in range(1, n-k+1):
+            if queue[0] < i:
+                tmp = queue.popleft()
+                if nums[tmp] == wmax:
+                    wmax = None if not queue else nums[queue[0]]  # need to add this, otherwise wrong, case2
+            while queue and nums[queue[-1]] < nums[i+k-1]:
+                queue.pop()
+            queue.append(i+k-1)
+            wmax = max(wmax, nums[i+k-1]) if wmax is not None else nums[i+k-1]
+            res.append(wmax)
+        return res
+
+
+class Solution1:
     """
     Key idea is that when a big number comes into the window, the smaller nums before it 
     does not need to be save, can be removed from the stack. So the deque is keeping a 
@@ -79,7 +137,7 @@ class SolutionTester(unittest.TestCase):
     def setUp(self):
         self.sol = Solution()
 
-    def test_case2(self):
+    def test_case02(self):
         nums = [1,2,7,7,2,10,3,4,5]
         k = 2
         answer = [2,7,7,7,10,10,4,5]  # wrong output [2,7,7,7,10,10,3,4]
@@ -91,6 +149,13 @@ class SolutionTester(unittest.TestCase):
         nums = [1,2,7,7,8]
         k = 3
         answer = [7,7, 8]
+        result = self.sol.maxSlidingWindow(nums, k)
+        self.assertEqual(answer, result)
+
+    def test_case3(self):   #=====>
+        nums = []
+        k = 0
+        answer = []
         result = self.sol.maxSlidingWindow(nums, k)
         self.assertEqual(answer, result)
 
