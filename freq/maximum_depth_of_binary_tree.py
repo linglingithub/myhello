1a84fbb5a43fb28f94c1fdd73fa8445a9a32eb2c
Ling Lin <linglingithub@users.noreply.github.com>
2017-11-03 11:01:56 -0700

    update

--- freq/maximum_depth_of_binary_tree.py
+++ freq/maximum_depth_of_binary_tree.py
@@ -28,6 +28,63 @@ from util.tree_node import TreeNode
 #         self.right = None
 
 class Solution(object):
+    def maxDepth(self, root):
+        """
+        recursive way
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+    def maxDepth1(self, root):
+        """
+        BFS way
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        res = 0
+        level = [root]
+        while level:
+            res += 1
+            children = []
+            for node in level:
+                if node.left:
+                    children.append(node.left)
+                if node.right:
+                    children.append(node.right)
+            level = children
+        return res
+
+    def maxDepth2(self, root):
+        """
+        DFS way
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        res = 0
+        depth = 0
+        stack = [(root, 1)]
+        node = root
+        while stack or node:
+            while node:
+                stack.append((node, depth + 1))
+                depth += 1
+                node = node.left
+
+            if stack:  # can't use while
+                node, depth = stack.pop()
+                res = max(res, depth)
+                node = node.right  # go to right without checking if None
+
+        return res
+
+class Solution1(object):
     def maxDepth(self, root): #iterative way, 72ms, 38%
         """
         :type root: TreeNode
@@ -78,7 +135,12 @@ def main():
 if __name__ == "__main__":
     main()
 
-
+"""
+Three ways
+(1) recursive
+(2) BFS
+(3) DFS
+"""
 
 #-*- coding:utf-8 -*-
 #coding=utf-8
