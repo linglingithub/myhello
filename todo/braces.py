55605ae2f889c38cca4df506fe45a843b3578198
Ling Lin <linglingithub@users.noreply.github.com>
2017-11-08 10:32:58 -0800

    update

--- todo/braces.py
+++ todo/braces.py
@@ -10,12 +10,47 @@ import unittest
 
 
 class Solution(object):
-    def searchInsert(self, nums):
+    def valid_brace(self, strs):
         """
         :type nums: List[int]
         :type target: int
         :rtype: int
         """
+        if not strs:
+            return []
+        res = []
+        for onestr in strs:
+            if self.check_valid(onestr):
+                res.append("YES")
+            else:
+                res.append("NO")
+        return res
+
+    def check_valid(self, nums):
+        stack = []
+        lefts = "{[("
+        rights = "}])"
+        pairs = {
+            "]": "[",
+            "}": "{",
+            ")": "(",
+        }
+        for char in nums:
+            if char in lefts:
+                stack.append(char)
+                continue
+            if char in rights:
+                if not stack:
+                    return False
+                top_char = stack.pop()
+                if pairs[char] != top_char:
+                    return False
+                continue
+            # for not valid char in input
+            return False
+        return True
+
+
 
 
 class SolutionTester(unittest.TestCase):
@@ -25,7 +60,7 @@ class SolutionTester(unittest.TestCase):
     def test_case1(self):
         nums = ["{}[]()", "{[}]}", "({[]})", "{)["]
         answer = ["YES", "NO", "YES", "NO"]
-        result = self.sol.searchInsert(nums)
+        result = self.sol.valid_brace(nums)
         self.assertEqual(answer, result)
 
 
