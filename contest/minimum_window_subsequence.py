ca1d0db802249b4b78dd858683df025b2e742c49
Ling Lin <linglingithub@users.noreply.github.com>
2017-11-16 20:01:01 -0800

    update

--- contest/minimum_window_subsequence.py
+++ contest/minimum_window_subsequence.py
@@ -3,18 +3,39 @@
 import unittest
 
 """
+727. Minimum Window Subsequence My SubmissionsBack to Contest
+Virtual User Accepted: 0
+Virtual User Tried: 0
+Virtual Total Accepted: 0
+Virtual Total Submissions: 0
+Difficulty: Hard
+Given strings S and T, find the minimum (contiguous) substring W of S, so that T is a subsequence of W.
 
+If there is no such window in S that covers all characters in T, return the empty string "". If there are multiple such 
+minimum-length windows, return the one with the left-most starting index.
+
+Example 1:
+Input: 
+S = "abcdebdde", T = "bde"
+Output: "bcde"
+Explanation: 
+"bcde" is the answer because it occurs before "bdde" which has the same length.
+"deb" is not a smaller window because the elements of T in the window must occur in order.
+Note:
+
+All the strings in the input will only contain lowercase letters.
+The length of S will be in the range [1, 20000].
+The length of T will be in the range [1, 100].
 
 """
 
 
-
 class Solution(object):
-    def testm(self, nums):
+    def minWindow(self, S, T):
         """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
+        :type S: str
+        :type T: str
+        :rtype: str
         """
 
 
