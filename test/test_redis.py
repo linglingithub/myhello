30c2f00ed3fae58d426c9bd7b070d9805d99a853
Ling Lin <linglingithub@users.noreply.github.com>
2018-05-15 18:02:43 -0700

    u

--- test/test_redis.py
+++ test/test_redis.py
@@ -12,6 +12,28 @@ r.set('foo', 'bar')
 r.set('foo', 123)
 print(r.get("foo"))
 
+
+# ======================= Pipelines
+
+r = redis.Redis()
+r.set('bing', 'baz')
+# Use the pipeline() method to create a pipeline instance
+pipe = r.pipeline()
+# The following SET commands are buffered
+pipe.set('foo', 'bar')
+pipe.get('bing')
+# the EXECUTE call sends all buffered commands to the server, returning
+# a list of responses, one for each command.
+tmp = pipe.execute()
+print(tmp)
+# For ease of use, all commands being buffered into the pipeline return the pipeline object itself. Therefore calls can be chained like:
+tmp = pipe.set('foo', 'bar').sadd('faz', 'baz').incr('auto_number').execute()
+# expect [True, True, 6]
+print(tmp)
+#n addition, pipelines can also ensure the buffered commands are executed atomically as a group. This happens by default.
+# If you want to disable the atomic nature of a pipeline but still want to buffer commands, you can turn off transactions.
+pipe = r.pipeline(transaction=False)
+
 """
 http://zhuhengwei.com/2016/01/27/Redis%E5%AD%98%E5%82%A8%E5%A4%8D%E6%9D%82%E7%B1%BB%E5%9E%8B%E7%9A%84%E6%80%9D%E8%80%83/
 
