924711d5b661cc6965903dcf3dc944b6f7ca2b09
linglingithub <linglin.mail@gmail.com>
2018-07-13 18:50:34 -0700

    'u'

--- jing/trie_wildcard_match.py
+++ jing/trie_wildcard_match.py
@@ -1,3 +1,33 @@
+from util.trie_node import TrieNode
 
+def match(root, word):
+    return _helper(root, word, 0)
 
-def match():
+def _helper(root, word, idx):
+    if not root:
+        return False
+    if idx >= len(word):
+        return root.is_word
+    char = word[idx]
+    if char != '*':
+        return _helper(root.children.get(char), word, idx + 1)
+    # deal with when current char is *
+    no_char_match = _helper(root, word, idx + 1)
+    if no_char_match:
+        return True
+    for dict_char, child in root.children.items():
+        if _helper(child, word, idx):
+            return True
+    return False
+
+root = TrieNode()
+words = ["abc", "pad", "dad", "pxyzd", "pd", "ad", "cd"]
+for word in words:
+    TrieNode.add_word(root, word)
+
+target = "*p*d*" # True
+target2 = "p**d" # True
+target3 = "xy"  # False
+target4 = "a*c*" # True
+
+print(match(root, target))
--- util/trie_node.py
+++ util/trie_node.py
@@ -1,6 +1,20 @@
 class TrieNode:
     def __init__(self):
         self.is_word = False
-        children = {}
+        self.children = {}
 
     @staticmethod
+    def add_word(root, word):
+        for char in word:
+            if char not in root.children:
+                root.children[char] = TrieNode()
+            root = root.children[char]
+        root.is_word = True
+
+    @staticmethod
+    def has_word(root, word):
+        for char in word:
+            root = root.children.get(char)
+            if not root:
+                return False
+        return root.is_word
